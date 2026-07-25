db99cf5077712810c9efa7b671b9ab865d48a23b
David Ruiz <davidruizdeveloper@gmail.com>
2026-07-25 11:42:19 -0500

    refactor player animation

--- game/actors/player/player.gd
+++ game/actors/player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
-@onready var animated_sprite_2d = $AnimatedSprite2D
 
 const SPEED = 300.0
-var facing := "down"
+var last_direction: Vector2 = Vector2.RIGHT
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
 	process_movement()
@@ -12,32 +12,19 @@ func process_movement() -> void:
 	var direction = Input.get_vector("player_move_left", "player_move_right", "player_move_up", "player_move_down")
 	
 	velocity = direction * SPEED
-	play_animation(direction)
+	process_animation(direction)
 	
-func play_animation(dir: Vector2) -> void:
-	if dir.x > 0:
-		facing = "right"
-		animated_sprite_2d.flip_h = false
-		animated_sprite_2d.play("run_right")
-	elif dir.x < 0:
-		facing = "left"
-		animated_sprite_2d.flip_h = true
-		animated_sprite_2d.play("run_right")
-	elif dir.y > 0:
-		facing = "down"
-		animated_sprite_2d.play("run_down")
-	elif dir.y < 0:
-		facing = "up"
-		animated_sprite_2d.play("run_up")
+func process_animation(direction)-> void:
+	if velocity != Vector2.ZERO:
+		play_animation("run", direction)
 	else:
-		match facing:
-			"right":
-				animated_sprite_2d.flip_h = false
-				animated_sprite_2d.play("idle_right")
-			"left":
-				animated_sprite_2d.flip_h = true
-				animated_sprite_2d.play("idle_right")
-			"up":
-				animated_sprite_2d.play("idle_up")
-			"down":
-				animated_sprite_2d.play("idle_down")
+		play_animation("idle", direction)
+		
+func play_animation(prefix: String, dir: Vector2) -> void:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix + "_right")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
+	elif dir.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
